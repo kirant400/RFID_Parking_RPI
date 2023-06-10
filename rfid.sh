da0b76853b968b10283eaa1b17287a46286d5245
Kirant400 <kirant400@gmail.com>
2023-06-10 14:13:27 +0400

    Create rfid.sh

---
+++ rfid.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+COMMAND="/usr/bin/python3 /home/rpi/pi-rfid/Read.py"
+LOGFILE=restart.txt
+
+writelog() {
+  now=`date`
+  echo "$now $*" >> $LOGFILE
+}
+
+writelog "Starting"
+while true ; do
+  $COMMAND
+  writelog "Exited with status $?"
+  writelog "Restarting"
+done
+
